e923a9d8329b153725a120f0cb407a8e337d18bd
Ted Satcher <ted.satcher@gmail.com>
2019-09-25 15:52:52 -0500

    Solved the bind notation problem

--- Pg867Ex/src/Lib.hs
+++ Pg867Ex/src/Lib.hs
@@ -4,7 +4,7 @@ import Data.Char
 import Control.Applicative
 
 cap :: [Char] -> [Char]
-cap xs = map toUpper xs
+cap xs = fmap toUpper xs
 
 rev :: [Char] -> [Char]
 rev xs = reverse xs
@@ -33,10 +33,42 @@ tupMdo = do
   b <- rev
   return ((,) a b)
 
--- :t (>>=) :: m a -> (a -> m b) ->   m  b
---                               -> (a,) b
-tupMJ :: [Char] -> ([Char], [Char])
-tupMJ = undefined
+
+--
+-- This one took awhile, but the breakthrough
+-- happened when I asked
+-- :t (,) "joe" :: b -> ([Char], b)
+-- and I realized this was the right side of bind.
+--
+-- How to write this in point free...
+tupMB :: [Char] -> ([Char], [Char])
+tupMB s = ("", rev s) >>= (,) (cap s)
+
+
+-- This was other attempts, but it was getting complicated...
+-- :t (>>=) ((,) cap) :: (([Char] -> [Char], b1) -> b1 -> b2) -> b1 -> b2
+g :: (([Char] -> [Char], b1) -> b1 -> b2) -> b1 -> b2
+g = undefined
+
+g' :: ([Char] -> [Char], [Char]) -> [Char] -> ([Char], [Char])
+g' = undefined
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+-- Back and forth between bind and do notations
+mFunc :: Int -> Maybe Int
+mFunc x
+      | x < 0 = Nothing
+      | otherwise = Just (x + 3)
+
+
+bindIt :: Maybe Int -> Maybe Int
+bindIt jx = jx >>= mFunc
+
+doIt :: Maybe Int -> Maybe Int
+doIt jx = do
+  x <- jx
+  mFunc x
