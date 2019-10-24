48fb8534a0d90dd16783877a8a406e95cda551a6
Ted Satcher <ted.satcher@gmail.com>
2019-10-24 16:27:57 -0500

    Added another note.

--- InlineEx/src/ReaderType.hs
+++ InlineEx/src/ReaderType.hs
@@ -1,4 +1,7 @@
 module ReaderType where
 
+-- r is the left-most type of the function that Reader holds
+-- a is the type that results when we apply a type r to the function
+-- inside of reader.
 newtype Reader r a =
     Reader {runReader :: (r -> a)}
